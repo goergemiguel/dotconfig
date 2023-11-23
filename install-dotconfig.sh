d023d792726d081a1d287a980b1ef5d6a3dca617
Goerge Miguel <goergemiguel21@gmail.com>
2023-11-23 21:39:25 +0800

    Update install-dotconfig.sh

--- install-dotconfig.sh
+++ install-dotconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # ------------------------------------------
-# Script to prerequisites for my dotfiles
+# Script to installing prerequisites for my dotfiles
 # ------------------------------------------
 
 # Install Homebrew

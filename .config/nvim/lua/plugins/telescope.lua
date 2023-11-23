cb2efd5a0249b0402ca9b7b12d8d765ed85b8692
goergemiguel <goergemiguel21@gmail.com>
2023-11-23 19:18:08 +0800

    fix(neovim): exlucde node_modules in telescope search

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -16,6 +16,7 @@ return {
 							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
 						},
 					},
+					file_ignore_patterns = { "node_modules" },
 				},
 			})
 		end,

bcd6bc57cbb50d7819f1123ea5620a79338b7bd8
goergemiguel <goergemiguel21@gmail.com>
2023-12-21 11:15:29 +0800

    chore(neovim): rename lsp plugin file; move all editing plugins into one file;

--- .config/nvim/lua/plugins/comment.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"numToStr/Comment.nvim",
-	lazy = true,
-	event = "CursorMoved",
-	config = function()
-		require("Comment").setup()
-	end,
-}
--- .config/nvim/lua/plugins/nvim-surround.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"kylechui/nvim-surround",
-	lazy = true,
-	event = "BufRead",
-	config = function()
-		require("nvim-surround").setup()
-	end,
-}

8b29848ca44f5ebc2dfcbb179e76e8150b736234
goergemiguel <goergemiguel21@gmail.com>
2024-04-22 11:50:01 +0800

    feat: change to catppuccin color theme for both neovim and kitty terminal

--- .config/nvim/lua/plugins/appearance.lua
+++ .config/nvim/lua/plugins/appearance.lua
@@ -1,8 +1,16 @@
 return {
+	-- {
+	-- 	"EdenEast/nightfox.nvim",
+	-- 	config = function()
+	-- 		vim.cmd("colorscheme nightfox")
+	-- 	end,
+	-- },
 	{
-		"EdenEast/nightfox.nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
 		config = function()
-			vim.cmd("colorscheme nightfox")
+			vim.cmd("colorscheme catppuccin-mocha")
 		end,
 	},
 	{

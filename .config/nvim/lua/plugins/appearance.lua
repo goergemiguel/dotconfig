ba704ea618a36383038e05007f71acf8b1473e45
goergemiguel <goergemiguel21@gmail.com>
2024-04-28 23:51:39 +0800

    feat(neovim): update statusline to minimal

--- .config/nvim/lua/plugins/appearance.lua
+++ .config/nvim/lua/plugins/appearance.lua
@@ -55,68 +55,69 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		config = function()
-			-- Bubbles config for lualine
-			-- Author: lokesh-krishna
-			-- MIT license, see LICENSE for more details.
+			local function location()
+				local line = vim.fn.line(".")
+				local col = vim.fn.virtcol(".")
+				return string.format("Ln %d,Col %d", line, col)
+			end
+			local diagnostics = {
+				"diagnostics",
 
-			local colors = {
-				cyan = "#79dac8",
-				black = "#080808",
-				white = "#c6c6c6",
-				green = "#91F5AD",
-				grey = "#303030",
-				pink = "#8B1E3F",
-				orange = "#DB4C40",
-				yellow = "#E9B44C",
+				sources = { "nvim_diagnostic" },
+				sections = { "error", "warn" },
+
+				diagnostics_color = {
+					error = "Statusline",
+					warn = "Statusline",
+					info = "Statusline",
+					hint = "Statusline",
+				},
+				symbols = {
+					error = "" .. " ",
+					warn = "" .. " ",
+					info = "I",
+					hint = "H",
+				},
+				colored = false, -- Displays diagnostics status in color if set to true.
+				update_in_insert = false, -- Update diagnostics in insert mode.
+				always_visible = true, -- Show diagnostics even if there are none.
 			}
 
-			local bubbles_theme = {
-				normal = {
-					a = { fg = colors.black, bg = colors.green },
-					b = { fg = colors.white, bg = colors.grey },
-					c = { fg = colors.black, bg = colors.black },
-				},
-
-				insert = { a = { fg = colors.black, bg = colors.yellow } },
-				visual = { a = { fg = colors.black, bg = colors.cyan } },
-				replace = { a = { fg = colors.white, bg = colors.pink } },
-				command = { a = { fg = colors.black, bg = colors.orange } },
-
-				inactive = {
-					a = { fg = colors.white, bg = colors.black },
-					b = { fg = colors.white, bg = colors.black },
-					c = { fg = colors.black, bg = colors.black },
-				},
+			local colors = {
+				fg = "#76787d",
+				bg = "#252829",
 			}
 
 			require("lualine").setup({
 				options = {
-					theme = bubbles_theme,
-					component_separators = "|",
-					section_separators = { left = "", right = "" },
+					theme = {
+						normal = {
+							a = { fg = colors.fg, bg = colors.bg },
+							b = { fg = colors.fg, bg = colors.bg },
+							c = { fg = colors.fg, bg = colors.bg },
+						},
+						insert = { a = { fg = colors.fg, bg = colors.bg }, b = { fg = colors.fg, bg = colors.bg } },
+						visual = { a = { fg = colors.fg, bg = colors.bg }, b = { fg = colors.fg, bg = colors.bg } },
+						command = { a = { fg = colors.fg, bg = colors.bg }, b = { fg = colors.fg, bg = colors.bg } },
+						replace = { a = { fg = colors.fg, bg = colors.bg }, b = { fg = colors.fg, bg = colors.bg } },
+
+						inactive = {
+							a = { bg = colors.fg, fg = colors.bg },
+							b = { bg = colors.fg, fg = colors.bg },
+							c = { bg = colors.fg, fg = colors.bg },
+						},
+					},
 				},
 				sections = {
-					lualine_a = {
-						{ "branch", separator = { left = "" }, right_padding = 2, gui = "bold" },
+					lualine_a = { "branch" },
+					lualine_b = { "filename" },
+					lualine_c = {
+						diagnostics,
 					},
-					lualine_b = { "filename", "diagnostics" },
-					lualine_c = { "fileformat" },
 					lualine_x = {},
-					lualine_y = { "filetype", "progress", "location" },
-					lualine_z = {
-						{ "mode", separator = { right = "" }, left_padding = 2 },
-					},
+					lualine_y = { "filetype" },
+					lualine_z = { "progress" },
 				},
-				inactive_sections = {
-					lualine_a = { "filename" },
-					lualine_b = {},
-					lualine_c = {},
-					lualine_x = {},
-					lualine_y = {},
-					lualine_z = { "location" },
-				},
-				tabline = {},
-				extensions = {},
 			})
 		end,
 		lazy = true,

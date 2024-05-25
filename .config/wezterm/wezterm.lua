9935b89971ca7ec0f90a72494ecac63b31816b12
goergemiguel <goergemiguel21@gmail.com>
2024-05-25 15:12:31 +0800

    feat: add more config to wezterm terminal

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,7 +8,69 @@ config.font_size = 14
 
 config.window_decorations = "RESIZE"
 
-config.use_fancy_tab_bar = true
+config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 50
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.keys = {
+	{
+		key = "d",
+		mods = "CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "u",
+		mods = "CMD",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		key = "j",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Prev"),
+	},
+	{
+		key = "k",
+		mods = "CMD",
+		action = wezterm.action.ActivatePaneDirection("Next"),
+	},
+}
+
+local function get_current_working_dir(tab)
+	local current_dir = tab.active_pane and tab.active_pane.current_working_dir or { file_path = "" }
+	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+
+	return current_dir == HOME_DIR and "." or string.gsub(current_dir.file_path, "(.*[/\\])(.*)", "%2")
+end
+
+-- Set tab title to the one that was set via `tab:set_title()`
+-- or fall back to the current working directory as a title
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local index = tonumber(tab.tab_index) + 1
+	local custom_title = tab.tab_title
+	local title = get_current_working_dir(tab)
+
+	if custom_title and #custom_title > 0 then
+		title = custom_title
+	end
+
+	return string.format("  %s•%s  ", index, title)
+end)
+
+-- Set window title to the current working directory
+wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
+	return get_current_working_dir(tab)
+end)
 
 return config

554ba704f2fb3644badacba37defb24570cac91f
goergemiguel <goergemiguel21@gmail.com>
2024-05-25 10:14:26 +0800

    feat: make wezterm borderless

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,4 +6,9 @@ config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrains Mono", { weight = "Medium" })
 config.font_size = 14
 
+config.window_decorations = "RESIZE"
+
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+
 return config

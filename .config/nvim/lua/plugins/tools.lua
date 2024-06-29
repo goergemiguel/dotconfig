32c9b65033068d1eecc341440ebdd38020547a48
goergemiguel <goergemiguel21@gmail.com>
2024-06-29 09:21:10 +0800

    feat: add tool to prevent cursor from moving to start after yank

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -42,4 +42,15 @@ return {
 		event = "BufRead",
 		opts = {},
 	},
+	{
+		"svban/YankAssassin.nvim",
+		lazy = true,
+		event = "BufRead",
+		config = function()
+			require("YankAssassin").setup({
+				auto_normal = true, -- if true, autocmds are used. Whenever y is used in normal mode, the cursor doesn't move to start
+				auto_visual = true, -- if true, autocmds are used. Whenever y is used in visual mode, the cursor doesn't move to start
+			})
+		end,
+	},
 }

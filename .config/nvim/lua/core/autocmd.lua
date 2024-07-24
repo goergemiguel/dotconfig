53891f2a6028a88546e78c7e074d57c1ce15f01f
goergemiguel <goergemiguel21@gmail.com>
2024-07-24 15:37:54 +0800

    feat: Remove autoformat autocmd

--- .config/nvim/lua/core/autocmd.lua
+++ .config/nvim/lua/core/autocmd.lua
@@ -1,6 +1,6 @@
 local api = vim.api
 
 -- Ensure file format is set to Unix before saving any file
-vim.cmd([[
-  autocmd BufWritePre * set ff=unix
-]])
+-- vim.cmd([[
+--   autocmd BufWritePre * set ff=unix
+-- ]])

f6044002e581886a27d3e5b69e05cefab64ecfe2
goergemiguel <goergemiguel21@gmail.com>
2024-08-12 14:03:00 +0800

    feat: add tool to beautify markdown files

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -53,4 +53,16 @@ return {
 			})
 		end,
 	},
+	{
+		"OXY2DEV/markview.nvim",
+		ft = "markdown", -- If you decide to lazy-load anyway
+		dependencies = {
+			-- You will not need this if you installed the
+			-- parsers manually
+			-- Or if the parsers are in your $RUNTIMEPATH
+			"nvim-treesitter/nvim-treesitter",
+
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 }
